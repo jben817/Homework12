392eb980338ed0b98850faa8abd4fe840f719314
blue-eyed iguana <61255945+jben817@users.noreply.github.com>
2020-05-16 23:40:57 -0400

    created js file

---
+++ index.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootpass",
+    database: "employee_trackerDB"
+  });
+
+  
